d71d9f2073a73ca062275a7d6b84f2549563109c
Pawan Kharel <163934527+kharpaw@users.noreply.github.com>
2026-06-01 14:21:27 +0300

    Made all the file ready

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Login } from './Components/Sign-up/login.jsx';
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+
+}
+
+export default App;
---
+++ src/Components/Sign-up/login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './login.css'
+import user_icon from '../../Asset/person.png'
+import email_icon from '../../Asset/email.png'
+import password from '../../Asset/password.png'
+
+  
+  
+export const login = () => {
+  return (
+    <div className='container'>
+      
+    </div>
+  )
+}
